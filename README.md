b9948807e00d87fa76bab8d7c2632ee91bfde1fd
jongeun park <jongeun.park@coinplug.com>
2017-07-02 11:16:21 +0900

    구현 완료

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # node-steemit-randowhale-bot
+
+## 설치
+```
+$ npm install
+```
+
+
+## 설정
+```
+configs/user.json 에서 본인 계정명과 비밀번호를 입력하세요.
+{
+	"name": "USER_NAME",
+	"password": "USER_PASSWORD"
+}
+
+configs/config.json 에서 가격, 최소 보팅파워, 최대 보팅파워를 입력하세요.
+각 변수들의 의미는 다음과 같습니다.
+-monitoringPeriod: 트렌젝션 조회의 주기(단위: 초)
+-minVotingPower: 최소 보팅파워(단위: %)
+-maxVotingPower: 최대 보팅파워(단위: %)
+-price: 보팅 금액(현재 2.000, 1.000 과 같이 소수점 3자리까지 직접 입력해야 합니다.)
+lastVotingTimestamp: 시작 타임스템프, 봇이 동작하면서 가장 최근에 보팅한 시간이 기록됩니다. 초기 값은 -1 또는 현재 시간으로 설정하는 것이 좋습니다. 
+{
+    "monitoringPeriod": 5,
+    "minVotingPower": 0.5,
+    "maxVotingPower": 2.5,
+    "price": "2.000 SBD",
+    "lastVotingTimestamp": 1498886877000
+}
+
+```
+## 시작
+설정이 완료되면, 아래의 명령어로 시작할 수 있습니다. 
+```
+$ node randowhale-bot.js
+```
+
+## License
+MIT
---
+++ randowhale-bot.js
@@ -0,0 +1,151 @@
+/**
+ * https://steemit.com/@jongeun
+ */
+
+var fs = require('fs');
+var steem = require('steem');
+var log4js = require('log4js');
+var logger = log4js.getLogger('RANDOWHALE-BOT');
+logger.setLevel('DEBUG');
+
+var SECOND = 1000;
+var CONFIG_FILEPATH = "./configs/config.json";
+
+var config = require(CONFIG_FILEPATH);
+
+config.lastVotingTimestamp = Date.parse(new Date());
+var user = require('./configs/user.json');
+var wif = initUser();
+
+
+setInterval(function() {
+	config = require(CONFIG_FILEPATH);
+	
+	startBot();
+}, config.monitoringPeriod * SECOND);
+
+
+
+function startBot(){
+	
+	steem.api.getAccountHistory(user.name, 99999999, 20, function(err, getAccountHistoryResult) {
+		if(!err && getAccountHistoryResult){
+			for(var i=0; i<getAccountHistoryResult.length; i++){
+				availableTransaction = getAvailableTransaction(getAccountHistoryResult[i]);
+				if(availableTransaction){
+					logger.info('보팅 후보: '+availableTransaction.memo);
+					config.lastVotingTimestamp = availableTransaction.postCreatedAt;
+					var str = JSON.stringify(config, null, 4);
+					
+					fs.writeFile(CONFIG_FILEPATH, str, "utf8", function (err) {
+						
+						
+			        });
+					vote(availableTransaction);
+				}
+			}
+		}
+	});
+}
+
+function vote(postInfo){
+	var max = config.maxVotingPower * 100;
+	var min = config.minVotingPower * 100;
+	votingPower = Math.floor(Math.random()*(max-min+1)) +min;
+
+	
+	postInfo.votingPower = votingPower;
+	logger.info('보팅 파워: '+(votingPower/100));
+	
+	steem.broadcast.vote(wif, user.name, postInfo.author, postInfo.permlink, votingPower, function(err, voteResult) {
+		if(!err && voteResult){
+
+			logger.info('보팅 성공: '+postInfo.memo);
+			createComment(postInfo);
+		}else{
+			logger.info('보팅 실패: '+err);
+		}
+	});
+}
+
+function initUser(){
+	return steem.auth.toWif(user.name, user.password, 'posting');
+}
+function createComment(postInfo){
+	
+	var commentPermlink = steem.formatter.commentPermlink(postInfo.author, postInfo.permlink);
+	postInfo.votingPower /= 100;
+	body = 'This post received a '+postInfo.votingPower+'% upvote from @'+user.name+' thanks to @'+postInfo.author+'! For more information, [click here](https://steemit.com/steemit/@randowhale/introducing-randowhale-will-you-get-the-100-vote-give-it-a-shot)!';
+	console.log(body);
+	steem.broadcast.comment(wif,  postInfo.author, postInfo.permlink, user.name, commentPermlink, "", body, "", function(err, result) {
+		if(!err && result){
+			logger.info('리플 작성 성공: '+body);
+		}else{
+			logger.info('리플 작성 실패: '+err);
+		}
+	});
+	
+}
+
+function getAvailableTransaction(transaction){
+	var data = JSON.stringify(transaction);
+	var start = data.indexOf(',');
+	if(start < 0){
+		return null;
+	}
+	var end = data.lastIndexOf(']');
+	if(end < 0 || end < start){
+		return null;
+	}
+	
+	data = data.substring(start+1, end);
+	data = JSON.parse(data);
+	var postCreatedAt = Date.parse(data.timestamp);
+	if(data.trx_id == "0000000000000000000000000000000000000000"){
+		return null;
+	}
+	
+	data = JSON.stringify(data);
+	start = data.indexOf("transfer");
+	
+	if(start < 0){
+		return null;
+	}
+	end = data.lastIndexOf(']');
+	if(end < 0 || end < start){
+		return null;
+	}
+	data = data.substring(start+"transfer".length+2, end);
+	data = JSON.parse(data);
+	
+	if(data.amount != config.price){
+		return null;
+	}
+	
+	
+	if (config.lastVotingTimestamp >= postCreatedAt){
+		return null
+	}
+	var memo = data.memo;
+	var startMemo =memo.indexOf('@');
+	if(!memo || memo.length < 0 || startMemo < 0 ){
+		return null;
+	}
+	var endMemo = memo.lastIndexOf('/');
+	if(endMemo < 0){
+		return null;
+	}
+	var permlink = memo.substring(endMemo+1);
+	var author = memo.substring(startMemo+1, endMemo);
+	
+	var result = {
+		author : author,
+		permlink:permlink,
+		memo: memo,
+		postCreatedAt:postCreatedAt
+	}
+	
+	return result;
+
+}
+
